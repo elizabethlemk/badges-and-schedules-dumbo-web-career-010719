2f6f309572f6465c3f9ffb400bbcde93f2d80a40
ELIZABETH LE <elizabethlemk@yahoo.com>
2018-12-13 07:38:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  badges = []
+  array.each do |i|
+    badges << "Hello, my name is #{i}."
+  end
+  badges
+end
+
+def assign_rooms(array)
+  room_assignments = []
+  %w(array).each_with_index { |name, index| 
+  room_assignments["Hello, #{name}! You'll be assigned to room "] = index + 1 "!"}
+end
